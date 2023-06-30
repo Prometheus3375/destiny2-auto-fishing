e79e61ea8606221d3a7919e9ce4afd8c0d1335b4
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-06-30 19:21:41 +0300

    Update main script

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-from fisher import start_fishing
-from methods.image import ImageMethod
+from argparse import ArgumentParser
+
+from predefined.image import *
+
+DEFAULT_FISH_LIMIT = 50
 
 if __name__ == '__main__':
-    start_fishing(
-        ImageMethod.from_predefined(
-            screen_width=1920,
-            screen_height=1080,
-            localization='english',
-            key='E',
-            ),
-        # It was reported that fish can disappear if there is around 60 uncollected fish.
-        # Putting 50 to be safe.
-        fish_limit=50,
-        do_initial_cast=True,
-        )
+    def main():
+        localizations_str = ', '.join(name.split('_', 1)[0] for name in available_localizations)
+        keys_str = ', '.join(name.split('_', 1)[0] for name in available_keys)
+
+        parser = ArgumentParser(description='A script for automatic fishing in Destiny 2')
+        parser.add_argument('screen_width', type=int, help='the width of your screen in pixels')
+        parser.add_argument('screen_height', type=int, help='the height of your screen in pixels')
+        parser.add_argument(
+            'localization',
+            help='the localization you use in Destiny 2; case insensitive. '
+                 f'Supported localizations: {localizations_str}',
+            )
+        parser.add_argument(
+            'interact_key',
+            help='the interaction key you use in Destiny 2; case insensitive. '
+                 f'Supported keys: {keys_str}',
+            )
+        parser.add_argument(
+            'fish_limit',
+            nargs='?',
+            default=50,
+            type=float,
+            help='the maximum number of fish this script is allowed to catch. '
+                 'People reported that fish can disappear if there is around 60 uncollected fish. '
+                 f'Defaults to {DEFAULT_FISH_LIMIT}. Put inf to remove limit',
+            )
+        parser.add_argument(
+            '--skip_initial_cast',
+            '-sic',
+            action='store_false',
+            help='if present then the script will not cast fishing rod for the first time',
+            )
+
+        args = parser.parse_args()
+
+        from fisher import start_fishing
+        from methods.image import ImageMethod
+
+        start_fishing(
+            ImageMethod.from_predefined(
+                screen_width=args.screen_width,
+                screen_height=args.screen_height,
+                localization=args.localization,
+                key=args.interact_key,
+                ),
+            fish_limit=args.fish_limit,
+            do_initial_cast=args.skip_initial_cast,
+            )
+
+
+    main()
