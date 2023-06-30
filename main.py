f45df992cac2d6854da376acbbcccfde46e32085
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-06-30 19:46:44 +0300

    Better help for main script

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from argparse import ArgumentParser
+from collections import defaultdict
+from collections.abc import Iterable
 
 from predefined.image import *
 
 DEFAULT_FISH_LIMIT = 50
 
 if __name__ == '__main__':
+    def group_by_name(available: Iterable[str], /) -> Iterable[str]:
+        data = defaultdict(set)
+        for name_resolution in available:
+            name, _, resolution = name_resolution.partition('_')
+            data[name].add(resolution)
+
+        for name, resolutions in data.items():
+            resolutions_str = ', '.join(resolutions)
+            yield f'{name!r} ({resolutions_str})'
+
+
     def main():
-        localizations_str = ', '.join(name.split('_', 1)[0] for name in available_localizations)
-        keys_str = ', '.join(name.split('_', 1)[0] for name in available_keys)
+        localizations_str = ', '.join(group_by_name(available_localizations))
+        keys_str = ', '.join(group_by_name(available_keys))
 
         parser = ArgumentParser(description='A script for automatic fishing in Destiny 2')
         parser.add_argument('screen_width', type=int, help='the width of your screen in pixels')
@@ -15,12 +28,12 @@ if __name__ == '__main__':
         parser.add_argument(
             'localization',
             help='the localization you use in Destiny 2; case insensitive. '
-                 f'Supported localizations: {localizations_str}',
+                 f'Supported localizations with resolutions: {localizations_str}',
             )
         parser.add_argument(
             'interact_key',
             help='the interaction key you use in Destiny 2; case insensitive. '
-                 f'Supported keys: {keys_str}',
+                 f'Supported keys with resolutions: {keys_str}',
             )
         parser.add_argument(
             'fish_limit',
@@ -29,7 +42,7 @@ if __name__ == '__main__':
             type=float,
             help='the maximum number of fish this script is allowed to catch. '
                  'People reported that fish can disappear if there is around 60 uncollected fish. '
-                 f'Defaults to {DEFAULT_FISH_LIMIT}. Put inf to remove limit',
+                 f"Defaults to {DEFAULT_FISH_LIMIT}. Put 'inf' to remove limit",
             )
         parser.add_argument(
             '--skip_initial_cast',
