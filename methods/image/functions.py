13fe366a9761597daf76fd122c027a571e700f30
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-02 21:38:43 +0300

    Update functions

--- methods/image/functions.py
+++ methods/image/functions.py
@@ -1,23 +1,35 @@
-from PIL.Image import Image
+from PIL.Image import Image, open as _open_image
 from numpy import asarray, ndarray
 
 
-def calculate_averages(image: Image, /) -> tuple[ndarray, ndarray]:
+def open_image(path: str, /) -> Image:
     """
-    Takes an image, converts it to grayscale and returns average values for every row and column.
+    Takes a path to an image file and loads it into RAM.
     """
-    a = asarray(image.convert('L'), int)
-    return a.mean(0, int), a.mean(1, int)
+    return _open_image(path, 'r', ['png'])
 
 
-def calculate_difference(im1: Image, im2: Image, /) -> tuple[ndarray, ndarray]:
+def image_matrix(image: Image, /) -> ndarray:
+    """
+    Takes an image, converts it to grayscale and returns its matrix.
+    """
+    return asarray(image.convert('L'), int)
+
+
+def difference_images(im1: Image, im2: Image, /) -> ndarray:
     """
     Takes two images, converts them to grayscale and returns absolute difference between
-    their average values for every row and column.
+    their matrices. **Note**: matrices must have identical shape.
     """
-    x1, y1 = calculate_averages(im1)
-    x2, y2 = calculate_averages(im2)
-    return abs(x1 - x2), abs(y1 - y2)
+    return abs(image_matrix(im1) - image_matrix(im2))
 
 
-__all__ = 'calculate_averages', 'calculate_difference'
+def difference_matrix_image(matrix: ndarray, im2: Image, /) -> ndarray:
+    """
+    Takes a matrix and an image, converts the image to grayscale and evaluates its matrix.
+    Returns absolute difference between matrices. **Note**: matrices must have identical shape.
+    """
+    return abs(matrix - image_matrix(im2))
+
+
+__all__ = 'open_image', 'image_matrix', 'difference_images', 'difference_matrix_image'
