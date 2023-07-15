898da4e40bdc9a3eb1da02facf9cac5ad4f9530d
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-16 02:00:19 +0300

    Fix press_key

--- d2autofishing/controls.py
+++ d2autofishing/controls.py
@@ -3,9 +3,18 @@ from time import sleep
 # noinspection PyProtectedMember
 from pyautogui._pyautogui_win import _click, _keyDown, _keyUp, _mouseDown, _mouseUp, _position
 
+KEY_PRESS_DURATION = 0.05
+"""
+Sleep time between down and up operations inside function ``press_key``.
+"""
+
 
 def press_key(key: str, /):
     _keyDown(key)
+    # Sometimes Destiny 2 fails to capture key press
+    # if there is no sleep or sleep time is low.
+    # Value of 1 / 32 was failing sometimes, so I decided to put 0.05.
+    sleep(KEY_PRESS_DURATION)
     _keyUp(key)
 
 
