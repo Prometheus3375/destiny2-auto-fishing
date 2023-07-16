7ff57ee47d841937bf6905c5b61e0fa769adc67c
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-16 03:03:12 +0300

    Use floor division instead of rounding

--- d2autofishing/functions.py
+++ d2autofishing/functions.py
@@ -7,7 +7,7 @@ def current_datetime_ms_str() -> str:
     The result always have 3 digits for fraction part of seconds.
     """
     dt = datetime.now()
-    ms = round(dt.microsecond / 1000)
+    ms = dt.microsecond // 1000
     dt = dt.replace(microsecond=0)
     return f'{dt}.{ms:03}'
 
