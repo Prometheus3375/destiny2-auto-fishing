c4da0bf0ce13c23b4befba0da61f3488bb5b559b
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-17 05:55:14 +0300

    Rename attributes in anti-AFK

--- d2autofishing/anti_afk.py
+++ d2autofishing/anti_afk.py
@@ -18,30 +18,30 @@ class AntiAFK:
 
     :param action_period: time in seconds how often anti-AFK actions should be done.
       Defaults to 120, minimum value is 30.
-    :param no_fish_threshold: time in seconds for how long no fish can be caught.
-      After this threshold is passed, the system will continue to perform anti-AFK actions
-      despite stopped fishing.
+    :param no_cast_threshold: time in seconds for how long
+      the fishing rod is allowed not to be cast. After this threshold is passed,
+      the system will continue to perform anti-AFK actions despite stopped fishing.
       Defaults to 60, minimum value is 30.
     """
     __slots__ = (
         'action_period',
-        'no_fish_threshold',
-        '_last_time_fish_caught',
-        '_last_time_action_done',
+        'no_cast_threshold',
+        '_last_time_actions_done',
+        '_last_time_rod_cast',
         )
 
     def __init__(
             self,
             /,
             action_period: float = 120,
-            no_fish_threshold: float = 60,
+            no_cast_threshold: float = 60,
             ):
         assert isinstance(action_period, (float, int)) and action_period >= 30
-        assert isinstance(no_fish_threshold, (float, int)) and no_fish_threshold >= 30
+        assert isinstance(no_cast_threshold, (float, int)) and no_cast_threshold >= 30
 
         self.action_period = action_period
-        self.no_fish_threshold = no_fish_threshold
-        self._last_time_fish_caught = self._last_time_action_done = perf_counter()
+        self.no_cast_threshold = no_cast_threshold
+        self._last_time_actions_done = self._last_time_rod_cast = perf_counter()
 
     def act(self, current_time: float, /):
         """
@@ -49,25 +49,25 @@ class AntiAFK:
         if anti-AFK actions were performed ``action_period`` seconds or more ago, performs them.
         Otherwise, does nothing.
         """
-        if current_time - self._last_time_action_done >= self.action_period:
-            self._last_time_action_done = current_time
+        if current_time - self._last_time_actions_done >= self.action_period:
+            self._last_time_actions_done = current_time
             press_key('f1')
             sleep(1)
             press_key('d')
             sleep(1)
             press_key('esc')
 
-    def act_based_on_catch(self, fish_caught: bool, /):
+    def act_based_on_catch(self, rod_cast: bool, /):
         """
-        Takes a boolean indicating whether fish was caught or not.
-        If fish was caught or the last catch was ``no_fish_threshold`` seconds or more ago,
+        Takes a boolean indicating whether the fishing rod is cast or not.
+        If the rod is cast or the last cast was ``no_cast_threshold`` seconds or more ago,
         then calls method ``act``. Otherwise, does nothing.
         """
         current_time = perf_counter()
-        if fish_caught:
-            self._last_time_fish_caught = current_time
+        if rod_cast:
+            self._last_time_rod_cast = current_time
 
-        if fish_caught or current_time - self._last_time_fish_caught >= self.no_fish_threshold:
+        if rod_cast or current_time - self._last_time_rod_cast >= self.no_cast_threshold:
             self.act(current_time)
 
     def loop_actions(self, /) -> Iterator[None]:
